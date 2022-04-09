4676ac6c91b8f3a846be57c5e7223e27e00034a3
ysirman <24975537+ysirman@users.noreply.github.com>
2022-04-09 15:25:31 +0900

    AWS Lambda 用に修正

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM rust:1.56 as develop-stage
 
+FROM ekidd/rust-musl-builder:1.57.0 AS builder
+# build
+USER rust
+ADD --chown=rust:rust . ./
+# lambda.zip作成のために必要
+RUN sudo apt-get update && sudo apt-get install zip
+RUN cargo build --release
+
+
+FROM rust:1.56 AS develop-stage
 WORKDIR /app
 # ホットリロード用
 RUN cargo install cargo-watch
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod utils;
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
 
-fn main() {
-    let price_and_rates = utils::get_price_and_rates();
-    // utils::send_email(&price_and_rates);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn func(_event: LambdaEvent<Value>) -> Result<Value, Error> {
+    tokio::task::spawn_blocking(|| {
+        let price_and_rates = utils::get_price_and_rates();
+        utils::send_email(&price_and_rates);
+    })
+    .await
+    .expect("Blocking task panicked");
+    Ok(json!({ "message": "success" }))
 }
